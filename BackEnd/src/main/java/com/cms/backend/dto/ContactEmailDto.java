375a3a9b346a262c49b680044aeb544ea70cd13c
Ibrahim5570 <8215@cch.edu.pk>
2026-04-23 15:18:45 +0500

    added contact email api

---
+++ BackEnd/src/main/java/com/cms/backend/dto/ContactEmailDto.java
@@ -0,0 +1,17 @@
+package com.cms.backend.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ContactEmailDto {
+    private Long id;
+
+    @NotBlank(message = "Email required!")
+    @Email(message = "Invalid Email format")
+    private String email;
+
+    @NotBlank(message = "Label required!")
+    private String label;
+}
