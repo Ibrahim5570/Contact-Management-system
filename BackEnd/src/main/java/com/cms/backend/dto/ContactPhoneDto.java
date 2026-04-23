893a4773704f4657c12b94de854c8ef6b91f87d5
Ibrahim5570 <8215@cch.edu.pk>
2026-04-23 15:34:35 +0500

    added contact phone dto

---
+++ BackEnd/src/main/java/com/cms/backend/dto/ContactPhoneDto.java
@@ -0,0 +1,15 @@
+package com.cms.backend.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class ContactPhoneDto {
+    private Long id;
+
+    @NotBlank(message = "Phone number required!")
+    private String phoneNumber;
+
+    @NotBlank(message = "Label required!")
+    private String label;
+}
