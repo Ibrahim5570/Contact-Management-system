766d7cab8b2e3a026940de453d73945d8af6fa95
Ibrahim5570 <8215@cch.edu.pk>
2026-05-20 16:02:10 +0500

    added placeholder Contacts.js and Profile.js for test run

---
+++ FrontEnd/cms-frontend/src/pages/Contacts.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Contacts() {
+    return <div>Contacts page coming soon</div>;
+}
+
+export default Contacts;
---
+++ FrontEnd/cms-frontend/src/pages/Profile.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Profile() {
+    return <div>Profile page coming soon</div>;
+}
+
+export default Profile;
