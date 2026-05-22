2a33684cbc6fdde50b8b750661458120975017b3
Ibrahim5570 <8215@cch.edu.pk>
2026-05-22 16:18:25 +0500

    changed @Data to @Getter and @Setter to fix 'ConcurrentModificationException'

--- BackEnd/src/main/java/com/cms/backend/entity/ContactEmail.java
+++ BackEnd/src/main/java/com/cms/backend/entity/ContactEmail.java
@@ -5,15 +5,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(name = "contact_emails")
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class ContactEmail
-{
+public class ContactEmail {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
